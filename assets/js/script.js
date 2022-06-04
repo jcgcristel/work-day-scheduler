3b7cf03fe257d74ea659fad3e2a5365f2fbf42a6
jcg <jcg.cristel@gmail.com>
2022-06-04 13:17:40 -0400

    added colour coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,11 @@
 // array to hold event information
 var eventsArr = [];
 
-var setEvent = function(id, event) {
-    $(`#${id}`).val(event);
-}
-
-// load events from localstorage 
-var load = function() {
-    eventsArr = JSON.parse(localStorage.getItem("events"))
-
-    // check if events in localstorage exist
-    if (!eventsArr) {
-        eventsArr = [];
-    }
-
-    for (var i = 0; i < eventsArr.length; i++) {
-        setEvent(eventsArr[i].id, eventsArr[i].event);
-    }
-}
+// css classes
+const hourClass = "col-2 p-3 hour text-right";
+const eventClass = "col-9 p-3 text-dark";
+const saveClass = "col-1 p-3 saveBtn d-flex justify-content-center align-items-center";
+const saveIcoPath = "./assets/images/save.png"
 
 // set current day at the top of the page
 var setCurrentDay = function() {
@@ -27,7 +15,35 @@ var setCurrentDay = function() {
         month: 'long', 
         day: '2-digit' });
     
+    // displays current day
     $("#currentDay").text(today);
+}
+
+var setEvent = function(id, event) {
+    $(`#${id}`).val(event);
+}
+
+// load events from localstorage 
+var load = function() {
+    eventsArr = JSON.parse(localStorage.getItem("events"))
+
+    // if local doesn't exist, create an array
+    if (!eventsArr) {
+        eventsArr = [];
+
+        for (var i = 0; i < 10; i++) {
+            eventsArr[i] = {
+               id: `row${i+8}-event`,
+               hour: i+8,
+               event: ""
+           }
+        } 
+    }
+
+    // set eventArr to hold localstorage array
+    for (var i = 0; i < eventsArr.length; i++) {
+        setEvent(eventsArr[i].id, eventsArr[i].event);
+    }
 }
 
 // create single hour block
@@ -39,17 +55,18 @@ var createHourBlock = function(hour, description, save) {
     hourText = luxon.DateTime.fromObject({hour: hour}).toLocaleString(luxon.DateTime.TIME_SIMPLE);
     
     // hour
-    var hourEl = $("<div>").addClass("col-2 p-3 hour text-right").text(hourText);
+    var hourEl = $("<div>").addClass(hourClass).text(hourText);
 
     // event
-    var eventEl = $("<textarea>").addClass("col-9 p-3 text-dark past")
-        .attr("id", `row${hour}-event`);
+    var eventEl = $("<textarea>").addClass(eventClass + " past")
+        .attr("id", `row${hour}-event`)
+        .attr("hour", hour);
 
     // save
-    var saveEl = $("<div>").addClass("col-1 p-3 saveBtn d-flex justify-content-center align-items-center")
+    var saveEl = $("<div>").addClass(saveClass)
         .attr("id", `row${hour}`);
     // save button
-    var saveIco = $("<img>").attr("src", "./assets/images/save.png");
+    var saveIco = $("<img>").attr("src", saveIcoPath);
     saveEl.append(saveIco);
 
     // append elements to event block
@@ -68,53 +85,68 @@ var createWorkSchedule = function() {
     }
 }
 
+var setBgColour = function() {
+    currentHour = luxon.DateTime.now().hour;
+    
+    for (var i = 0; i < eventsArr.length; i++) {
+        var eventEl = $(`#${eventsArr[i].id}`);        
+        
+        // convert event hour
+        eventHour = eventEl.attr("hour")
+        eventHour = luxon.DateTime.fromObject({hour: eventHour}).hour;
+        
+        // remove any time styling
+        $(eventEl).removeClass("past present future");
+        
+        if (eventHour < currentHour) {
+            $(eventEl).addClass("past");
+        }
+        
+        if (eventHour == currentHour) {
+            $(eventEl).addClass("present");
+        }
+        
+        if (eventHour > currentHour) {
+            $(eventEl).addClass("future");           
+        }        
+    }
+}
 
 setCurrentDay();
 createWorkSchedule();
 load();
+setBgColour();
 
-// check if event already exists in array, returning the id if it does
-var eventExists = function(object, list) {
+// update local storage
+var save = function() {
+    localStorage.setItem("events", JSON.stringify(eventsArr));
+}
+
+// get index of array
+var getIndex = function(object, list) {
     for (var i = 0; i < list.length; i++) {
         if (list[i].id == object.id) {
             return i;
         }
     }
-    return false;
-}
-
-// update local storage
-var save = function(id) {
-    console.log(localStorage.setItem("events", JSON.stringify(eventsArr)));
 }
 
 // clicking save button will save event
 $(".saveBtn").click( function(event) {
-    var rowID = $(this).attr("id");
-    var textID = `${rowID}-event`;
-    var textEl = $(`#${textID}`);
+    var rowID = $(this).attr("id"); // get row id
+    var textID = `${rowID}-event`; // find event id from row id
+    var textEl = $(`#${textID}`); // jQuery format of event id
 
     var event = {
         id: textID,
         event: textEl.val()
     }
 
-    var id = eventExists(event, eventsArr);
+    var id = getIndex(event, eventsArr);
+    
+    // update event array with current event value
+    eventsArr[id].event = event.event;
 
-     // checks if event already exists in array
-     if (id !== false) {
-        console.log("exists");
-        // updates event
-        eventsArr[id].event = event.event;
-        console.log(eventsArr);
-     }
-     else {
-        console.log("does not exists")
-        // creates new event
-        eventsArr.push(event);
-     }
-
-     console.log("saving")
-     save(event.id);
+    save();
 })
 
