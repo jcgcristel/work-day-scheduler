91a2887e702366d3c0cf6275c6ac4ab78a7988f3
jcg <jcg.cristel@gmail.com>
2022-06-04 01:54:27 -0400

    interact with event and save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// function to set current day at the top of the page
+// set current day at the top of the page
 var setCurrentDay = function() {
     // grabs current time in [Weekday, Month Day] format
     var today = luxon.DateTime.now().toLocaleString({ 
@@ -9,28 +9,32 @@ var setCurrentDay = function() {
     $("#currentDay").text(today);
 }
 
+// create single hour block
 var createHourBlock = function(hour, description, save) {
     // create hour block element
     var hourBlock = $("<div>").addClass("row");
 
     // convert hour int to hour format
-    hour = luxon.DateTime.fromObject({hour: hour}).toLocaleString(luxon.DateTime.TIME_SIMPLE);
+    hourText = luxon.DateTime.fromObject({hour: hour}).toLocaleString(luxon.DateTime.TIME_SIMPLE);
     
-    // create elements for hour block
-    var hourEl = $("<div>").addClass("col-2 p-3 hour text-right")
-        .text(hour);
-    var textEl = $("<div>").addClass("col-9 p-3 text-dark textarea past")
-        .attr("id", `text${hour}`);
-    var saveEl = $("<div>").addClass("col-1 p-3 saveBtn d-flex justify-content-center align-items-center");
-    
-    // save txt
-    var saveTxt = $("<i>").text("Save")
-        .attr("id", `save${hour}`);
-    saveEl.append(saveTxt);
+    // hour
+    var hourEl = $("<div>").addClass("col-2 p-3 hour text-right").text(hourText);
+
+    // event
+    var eventEl = $("<textarea>").addClass("col-9 p-3 text-dark past")
+        .attr("id", `row${hour}-event`)
+        .val("Texte");
+
+    // save
+    var saveEl = $("<div>").addClass("col-1 p-3 saveBtn d-flex justify-content-center align-items-center")
+        .attr("id", `row${hour}`);
+    // save button
+    var saveIco = $("<img>").attr("src", "./assets/images/save.png");
+    saveEl.append(saveIco);
 
     // append elements to hour block
     hourBlock.append(hourEl);
-    hourBlock.append(textEl);
+    hourBlock.append(eventEl);
     hourBlock.append(saveEl);
 
     // append hour block to page
@@ -46,3 +50,14 @@ var createWorkSchedule = function() {
 
 setCurrentDay();
 createWorkSchedule();
+
+// clicking save button will save event
+$(".saveBtn").click( function(event) {
+    var rowID = $(this).attr("id");
+    var textID = `${rowID}-event`;
+    var textEl = $(`#${textID}`);
+
+    console.log(textEl.val());
+    console.log("saving")
+})
+
