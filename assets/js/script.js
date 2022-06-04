0923b9306be177c4fad21dbaa9c4c399a0266ec7
jcg <jcg.cristel@gmail.com>
2022-06-03 21:29:42 -0400

    pull current day to display at the top and displays hour values in time_simple format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,31 @@
+// function to set current day at the top of the page
+var setCurrentDay = function() {
+    // grabs current time in [Weekday, Month Day] format
+    var today = luxon.DateTime.now().toLocaleString({ 
+        weekday: 'long',
+        month: 'long', 
+        day: '2-digit' });
+    
+    $("#currentDay").text(today);
+}
+
 var createHourBlock = function(hour, description, save) {
     // create hour block element
     var hourBlock = $("<div>").addClass("row");
 
+    // convert hour int to hour format
+    hour = luxon.DateTime.fromObject({hour: hour}).toLocaleString(luxon.DateTime.TIME_SIMPLE);
+    
     // create elements for hour block
     var hourEl = $("<div>").addClass("col-2 p-3 hour text-right")
         .text(hour);
-    var textEl = $("<div>").addClass("col-9 p-3 text-dark textarea past");
+    var textEl = $("<div>").addClass("col-9 p-3 text-dark textarea past")
+        .attr("id", `text${hour}`);
     var saveEl = $("<div>").addClass("col-1 p-3 saveBtn d-flex justify-content-center align-items-center");
     
     // save txt
-    var saveTxt = $("<i>").text("Save");
+    var saveTxt = $("<i>").text("Save")
+        .attr("id", `save${hour}`);
     saveEl.append(saveTxt);
 
     // append elements to hour block
@@ -27,4 +43,5 @@ var createWorkSchedule = function() {
     }
 }
 
+setCurrentDay();
 createWorkSchedule();
