c4768fd92d0b117b3c249278a552cee8d717ad95
jcg <jcg.cristel@gmail.com>
2022-06-04 02:14:01 -0400

    saves event to an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
-// function to set current day at the top of the page
+// array to hold event information
+var eventArr = [];
+
+// set current day at the top of the page
 var setCurrentDay = function() {
     // grabs current time in [Weekday, Month Day] format
     var today = luxon.DateTime.now().toLocaleString({ 
@@ -9,31 +12,35 @@ var setCurrentDay = function() {
     $("#currentDay").text(today);
 }
 
+// create single hour block
 var createHourBlock = function(hour, description, save) {
     // create hour block element
     var hourBlock = $("<div>").addClass("row");
 
     // convert hour int to hour format
-    hour = luxon.DateTime.fromObject({hour: hour}).toLocaleString(luxon.DateTime.TIME_SIMPLE);
+    hourText = luxon.DateTime.fromObject({hour: hour}).toLocaleString(luxon.DateTime.TIME_SIMPLE);
     
-    // create elements for hour block
-    var hourEl = $("<div>").addClass("col-2 p-3 hour text-right")
-        .text(hour);
-    var textEl = $("<div>").addClass("col-9 p-3 text-dark textarea past")
-        .attr("id", `text${hour}`);
-    var saveEl = $("<div>").addClass("col-1 p-3 saveBtn d-flex justify-content-center align-items-center");
-    
-    // save txt
-    var saveTxt = $("<i>").text("Save")
-        .attr("id", `save${hour}`);
-    saveEl.append(saveTxt);
+    // hour
+    var hourEl = $("<div>").addClass("col-2 p-3 hour text-right").text(hourText);
 
-    // append elements to hour block
+    // event
+    var eventEl = $("<textarea>").addClass("col-9 p-3 text-dark past")
+        .attr("id", `row${hour}-event`)
+        .val("Texte");
+
+    // save
+    var saveEl = $("<div>").addClass("col-1 p-3 saveBtn d-flex justify-content-center align-items-center")
+        .attr("id", `row${hour}`);
+    // save button
+    var saveIco = $("<img>").attr("src", "./assets/images/save.png");
+    saveEl.append(saveIco);
+
+    // append elements to event block
     hourBlock.append(hourEl);
-    hourBlock.append(textEl);
+    hourBlock.append(eventEl);
     hourBlock.append(saveEl);
 
-    // append hour block to page
+    // append event block to page
     $(".container").append(hourBlock);
 }
 
@@ -44,5 +51,39 @@ var createWorkSchedule = function() {
     }
 }
 
+
 setCurrentDay();
 createWorkSchedule();
+
+// check if event already exists in array
+var eventExists = function(object, list) {
+    for (var i = 0; i < list.length; i++) {
+        if (list[i].id == object.id) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// clicking save button will save event
+$(".saveBtn").click( function(event) {
+    var rowID = $(this).attr("id");
+    var textID = `${rowID}-event`;
+    var textEl = $(`#${textID}`);
+
+    var event = {
+        id: textID,
+        event: textEl.val()
+    }
+
+     // checks if event already exists in array
+     if (eventExists(event, eventArr)) {
+        // exits -> updates event
+        textEl.attr("event", textEl.val());
+     }
+     else {
+        // !exist -> add new event
+        eventArr.push(event);
+     }
+})
+
